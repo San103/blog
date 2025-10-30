19564c3f401c390e90de16f2da9d9a34a466e506
Jessan P <palbanjessan15@gmail.com>
2025-10-30 11:50:17 +0800

    change title

--- lib/site.ts
+++ lib/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  name: "Magic UI Blog",
+  name: "San's Blog",
   url: "https://blog-magicui.vercel.app",
   description:
     "Modern React and web development tutorials, insights, and best practices.",
